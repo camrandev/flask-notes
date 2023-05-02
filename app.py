ae80abc7a34833d7635f87ba307ecf9a1c6b8c53
Crynowecer0 <crynowecer11@gmail.com>
2023-05-02 14:52:55 -0400

    finish setting up dev environment and boilerplate

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import os
 
 from flask import Flask, render_template, redirect, flash, jsonify, request
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db, db, Cupcake, DEFAULT_IMAGE
+from models import connect_db, db
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "secret"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    "DATABASE_URL", "postgresql:///cupcakes")
+    "DATABASE_URL", "postgresql:///notes")
 
 connect_db(app)
 
